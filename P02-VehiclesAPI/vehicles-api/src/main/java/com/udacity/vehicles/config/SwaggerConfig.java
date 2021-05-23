1230a34883b6b1a4c2a209d7de3250755baeae19
p1tagora <calin.pelcea89@gmail.com>
2021-05-23 16:07:07 +0200

    Add Swagger-Ui to this service

---
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.udacity.vehicles.config;
+
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Vehicles API",
+                "REST API to maintain vehicle data and to provide a complete view of vehicle details including price and address (obtained from the location and pricing services).",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Calin Pelcea", "https://github.com/p1tagora", "calin.pelcea89@gmail.com"),
+                "License of API", "Dummy license", Collections.emptyList());
+    }
+}
